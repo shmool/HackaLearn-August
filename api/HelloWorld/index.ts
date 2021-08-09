14abb6751d9375bd55e30df1be956057b0f5139d
shmool <shmuelaj@gmail.com>
2021-08-09 14:07:39 +0300

    add context log name

--- api/HelloWorld/index.ts
+++ api/HelloWorld/index.ts
@@ -7,6 +7,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
+    context.log('Name is: ', name);
+
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: { responseMessage }
